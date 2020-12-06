bfc5c4371d7699c95220f5ca33f3222b9c4a35a9
George Cimpureanu <georgecimpureanu@yahoo.com>
2020-12-06 15:38:48 +0200

    Updated utils

--- Development/utils.py
+++ Development/utils.py
@@ -4,3 +4,13 @@ simptoms = ['tuse', 'dispnee', 'febra', 'frison',
             'oboseala', 'diaree', 'scaun',
             'temp', '37', '38', '39', '40', 'muscular', 
             'nas infundat', 'anosmie']
+
+
+diagnostic_simptoms_lower = ['respir', 'bronhopneum', 'pneumopatie', 'frison',
+                            'frisoane', 'tuse', 'dispnee', 'febra', 'mialg',
+                            'oboseala', 'cefalee', 'odinofag', '38.', '38,',
+                            '39.', '39,', '40.', '40,', 'temp']
+
+diagnostic_simptoms_med = ['susp', 'covid19', 'covid 19', 'cov19', 'cov-2', 'cov 2', 'sars']
+
+diagnostic_simptoms_higher = ['confirm']
